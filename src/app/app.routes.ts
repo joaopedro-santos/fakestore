3ae7c202b735d066fa6ac47d7f51ab7caff08810
João Pedro Santos <37111524+joaopedro-santos@users.noreply.github.com>
2026-07-17 20:04:50 -0300

    Primeiro commit
    Criação do projeto e estruturação das pastas

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('../features/home/home.routes').then((module) => module.HOME_ROUTES),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  appName: 'suzantur-products',
+  apiUrl: 'https://fakestoreapi.com',
+} as const;
